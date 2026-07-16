ca0d9a4d6ad73b888ae07987a675c9f2d030fe31
Aksamit918 <aksamit918@gmail.com>
2026-07-16 20:41:29 +0300

    feat: add  system tables

--- src/main/resources/db/migration/V3__sys_tables_default_balance.sql
+++ src/main/resources/db/migration/V3__sys_tables_default_balance.sql
@@ -10,7 +10,6 @@ ALTER TABLE game_tables
 INSERT INTO game_tables (
     id, name, small_blind, big_blind, min_players, max_players, is_private, is_system, created_at, min_buy_in, max_buy_in
 ) VALUES
-      -- 10/20 Tables (Min: 200, Max: 2,000)
       (gen_random_uuid(), 'Последний додеп', 10, 20, 2, 9, FALSE, TRUE, NOW(), 200, 2000),
       (gen_random_uuid(), 'Отчаяние', 10, 20, 2, 9, FALSE, TRUE, NOW(), 200, 2000),
       (gen_random_uuid(), 'Безысходность', 10, 20, 2, 9, FALSE, TRUE, NOW(), 200, 2000),
