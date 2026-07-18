e05bfa7077af56fc81efad22ee16591bd6e5248d
Aksamit918 <aksamit918@gmail.com>
2026-07-18 13:48:55 +0300

    fix: resolve CORS 500 error in UserController

--- src/main/java/com/poker/controller/UserController.java
+++ src/main/java/com/poker/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "*")
+@CrossOrigin(originPatterns = "*", allowCredentials = "true")
 @RequiredArgsConstructor
 public class UserController {
 
